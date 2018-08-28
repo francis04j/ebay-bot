b56b1764d99164f53d30cefd1fedb679c6adf7b4
francis.adediran <Francis.Adediran@perfectchannel.com>
2018-08-28 17:54:25 +0100

    initial rule

--- handler.ts
+++ handler.ts
@@ -1,13 +1,23 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+
 
 export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
+  AWS.config.update({region: 'eu-west-1'});
+  const cwevents = new AWS.CloudWatchEvents({apiVersion: '2015-10-07'});
+
+  const params = {
+    Name: 'DEMO_EVENT',
+    RoleArn: 'IAM_ROLE_ARN',
+    ScheduleExpression: 'rate(5 minutes)',
+    State: 'ENABLED'
   };
 
-  cb(null, response);
-}
+  cwevents.putRule(params, function(err, data) {
+    if (err) {
+      console.log('Error', err);
+    } else {
+      console.log('Success', data.RuleArn);
+    }
+  });
+};
